f50b08734fc0f28c305059e2436df6df88826b03
Andreas Hildebrandt <andreas.hildebrandt@uni-mainz.de>
2017-03-26 00:29:39 +0100

    Updated to HDP 2.5

--- ambari-server/shell/install-cluster.sh
+++ ambari-server/shell/install-cluster.sh
@@ -3,6 +3,7 @@
 ./ambari-shell.sh << EOF
 blueprint add --url https://raw.githubusercontent.com/sequenceiq/ambari-rest-client/2.2.11/src/main/resources/blueprints/multi-node-hdfs-yarn
 blueprint add --url https://raw.githubusercontent.com/sequenceiq/ambari-rest-client/2.2.11/src/main/resources/blueprints/single-node-hdfs-yarn
+blueprint add --file /tmp/multi-node-hdfs-yarn-2.5
 cluster build --blueprint $BLUEPRINT
 cluster autoAssign
 cluster create --exitOnFinish true
--- kubernetes/install-cluster.sh
+++ kubernetes/install-cluster.sh
@@ -1,4 +1,4 @@
-export BLUEPRINT=multi-node-hdfs-yarn
+export BLUEPRINT=multi-node-hdfs-yarn-2.5
 export EXPECTED_HOST_COUNT=5
 /tmp/install-cluster.sh
 
